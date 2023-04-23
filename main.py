d2dc8baceb8707f4c8afea161acc85c0336f954f
pathum1 <nerangenpathum@gmail.com>
2023-04-23 08:34:49 +0530

    first commit

---
+++ main.py
@@ -0,0 +1,158 @@
+import toml
+import obsws_python as obs
+from gui import create_gui
+from dota2_gsi import run  # Import the run function from dota2_gsi.py
+import threading
+import time  # Import the time module
+from utils import save_scenarios, load_scenarios
+import logging
+from tkinter import Tk
+
+import sys
+import os
+
+
+sys.stdout = open(os.devnull, 'w')
+sys.stderr = open(os.devnull, 'w')
+
+# Configure the logger
+logging.basicConfig(filename='app.log', filemode='w', format='%(name)s - %(levelname)s - %(message)s', level=logging.DEBUG)
+
+# Log a message
+logging.warning('This is a warning message')
+
+# Define a global variable to hold the previous game state
+previous_game_state = {}
+
+
+# Define a global variable to hold the list of scenarios
+scenarios = []
+scenarios = load_scenarios()
+
+# Load the configuration file
+config = toml.load('config.toml')
+
+# Get the connection details from the config file
+host = config['connection']['host']
+port = config['connection']['port']
+password = config['connection']['password']
+
+obs_running = True
+try:
+    client = obs.ReqClient(host=host, port=port, password=password)
+except ConnectionRefusedError:
+    obs_running = False
+    client = None
+
+# client = obs.ReqClient(host=host, port=port, password=password)
+
+# Load the list of scenarios from a file
+
+
+def process_data(client, data):
+    logging.debug(f"Data received by process_data: {data}")
+    global previous_game_state  # Use the global previous_game_state variable
+
+    # print("Processing data...")  # Add a print statement here
+    # print(f"1 - Incoming data: {data}")  # Add a print statement here
+    # Iterate over the list of scenarios
+    for scenario in scenarios:
+        logging.debug(f"Checking scenario: {scenario}")
+        logging.debug(f"Scenario event: {scenario['event']}")
+        logging.debug(f"'kills' in data: {'kills' in data}")
+        # Check if the incoming data matches the scenario
+        if scenario['event'] == 'Kill' and 'player' in data and 'kills' in data['player']:
+            # Check if the kill count has increased
+            if 'kills' not in previous_game_state or data['player']['kills'] > previous_game_state['kills']:
+                logging.debug("Kill count has increased")
+
+                # Get the sceneItemId for the selected source
+                response = client.get_scene_item_id(scenario['scene'], scenario['source'])
+                logging.debug(f"Response: {response}")
+                scene_item_id = response.scene_item_id
+                logging.debug(f"Scene Item ID: {scene_item_id}")
+                # Enable the corresponding source in OBS using its sceneItemId
+                client.set_scene_item_enabled(scenario['scene'], scene_item_id, True)
+                logging.debug(f"Enabled source: {scenario['source']}")
+
+                # Wait for 4 seconds
+                time.sleep(4)
+
+                # Disable the corresponding source in OBS using its sceneItemId
+                client.set_scene_item_enabled(scenario['scene'], scene_item_id, False)
+                logging.debug(f"Disabled source: {scenario['source']}")
+
+        elif scenario['event'] == 'Death' and 'player' in data and 'deaths' in data['player']:
+            # Check if the death count has increased
+            if 'deaths' not in previous_game_state or data['player']['deaths'] > previous_game_state['deaths']:
+                logging.debug("Death count has increased")
+
+                # Get the sceneItemId for the selected source
+                response = client.get_scene_item_id(scenario['scene'], scenario['source'])
+                scene_item_id = response.scene_item_id
+
+                # Enable the corresponding source in OBS using its sceneItemId
+                client.set_scene_item_enabled(scenario['scene'], scene_item_id, True)
+                logging.debug(f"Enabled source: {scenario['source']}")  # Add a print statement here
+
+                # Wait for 4 seconds
+                time.sleep(4)
+
+                # Disable the corresponding source in OBS using its sceneItemId
+                client.set_scene_item_enabled(scenario['scene'], scene_item_id, False)
+                logging.debug(f"Disabled source: {scenario['source']}")  # Add a print statement here
+
+        elif scenario['event'] == 'Assist' and 'player' in data and 'assists' in data['player']:
+            # Check if the assist count has increased
+            if 'assists' not in previous_game_state or data['player']['assists'] > previous_game_state['assists']:
+                logging.debug("Assist count has increased")  # Add a print statement here
+
+                # Get the sceneItemId for the selected source
+                response = client.get_scene_item_id(scenario['scene'], scenario['source'])
+                scene_item_id = response.scene_item_id
+
+                # Enable the corresponding source in OBS using its sceneItemId
+                client.set_scene_item_enabled(scenario['scene'], scene_item_id, True)
+                logging.debug(f"Enabled source: {scenario['source']}")  # Add a print statement here
+
+                # Wait for 4 seconds
+                time.sleep(4)
+
+                # Disable the corresponding source in OBS using its sceneItemId
+                client.set_scene_item_enabled(scenario['scene'], scene_item_id, False)
+                logging.debug(f"Disabled source: {scenario['source']}")  # Add a print statement here
+
+    # Update the previous game state with the current game state
+    if 'player' in data:
+        if 'kills' in data['player']:
+            previous_game_state['kills'] = data['player']['kills']
+            logging.debug(f"Previous game state updated: {previous_game_state}")
+        if 'deaths' in data['player']:
+            previous_game_state['deaths'] = data['player']['deaths']
+        if 'assists' in data['player']:
+            previous_game_state['assists'] = data['player']['assists']
+
+
+# Start the Flask app in dota2_gsi.py and pass in the client object and process_data function
+threading.Thread(target=run, args=(client, process_data)).start()  # Start the Flask app in a separate thread
+
+
+# whether the source is active
+if client is not None:
+    response1 = client.get_source_active('KILL_ALERT')
+else:
+    print("OBS is not running")
+
+# gets the list of scenes
+if client is not None:
+    response3 = client.get_scene_list()
+else:
+    print("OBS is not running")
+
+# prints an array of scenes
+# print(f"Scene list: {response3.scenes}")
+
+root = Tk()
+root.title("OBS Triggered ⚡")
+create_gui(client, scenarios, root, obs_running)
+
